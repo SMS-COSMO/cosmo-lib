8f895ed6436dc961705e864f8fa580fa35218e04
WFengYueWuQing <cmd_seme@163.com>
2025-12-07 13:58:42 +0800

    feat: 添加自动路由 add unplugin-vue-router dependency and configure auto-routing in the application

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">You did it!</h1>
-  <p class="mt-4">
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener" class="text-blue-500 hover:text-blue-700">vuejs.org</a> to read the
-    documentation
-  </p>
+  <RouterView />
 </template>
 
 <style scoped></style>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>
+      cosmo-lib包根页面
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
